d5d4ffe308619819aa316840d2a52ad4fb4d0ec5
flavian-jumba <flaviajumba@gmail.com>
2025-07-13 03:49:32 +0300

    made the changes

--- python_introduction/basic_operations.py
+++ python_introduction/basic_operations.py
@@ -5,7 +5,7 @@ number1 = 10,number2 = 5
 
 result = (number1 + number2)
 answ = (number1 * number2)
-diff = (number2 - number1)
+diff = (number1 - number2)
 
 print(f"The sum of {number1} and {number2} is {result}")
 print(f"The product of {number1} and {number2} is {answ}")
