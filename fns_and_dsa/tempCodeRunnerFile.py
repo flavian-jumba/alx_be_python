9f8e4423387c43388aa8bb404bedaaf52861d162
flavian-jumba <flaviajumba@gmail.com>
2025-07-29 20:43:12 +0300

    shopping list

---
+++ fns_and_dsa/tempCodeRunnerFile.py
@@ -0,0 +1,16 @@
+def perform_operation(num1:float,num2:float,operation:str) ->float:
+  if operation == 'add':
+    return num1 + num2
+  elif operation == "subtract":
+    return num1 - num2
+  elif operation == "multiply":
+    return num1 * num2
+  elif operation == "divide":
+    if num2 == 0:
+        raise ValueError("Cannot divide by zero.")
+    return num1 / num2
+       
+  else:
+        raise ValueError("Invalid operation. Choose 'add', 'subtract', 'multiply', or 'divide'.")   
+
+    
